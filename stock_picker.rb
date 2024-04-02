48af867791d2f5bd9fec4e88fed0ef82baff2396
AaronS83 <aaronsaldanha83@gmail.com>
2024-04-02 20:07:52 +0530

    Update stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -29,6 +29,29 @@ def stock_picker(days)
     return [max_front,max_last]
 end
 
+# other i/p
+# def stock_picker(prices)
+#     min_p = prices[0]
+#     min_i = 0
+  
+#     profit = 0
+#     days = [0, 0]
+  
+#     prices.each_with_index do |p, i|
+#       if p < min_p
+#         min_p = p
+#         min_i = i
+#         next
+#       end
+      
+#       if p - min_p > profit
+#         profit = p - min_p
+#         days = [min_i, i]
+#       end
+#     end
+    
+#     days
+#   end
 
-# p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,19,10])
 p stock_picker([19,13,10,5,3,6,4,2,1])
