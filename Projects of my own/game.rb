adb10488bc8bde098805f3bc6619eb55e414a6ec
AaronS83 <aaronsaldanha83@gmail.com>
2024-04-07 12:12:23 +0530

    Update to game.rb
    Gave proper getter and setter to instance variables
    Added die roll for attack turn

--- Projects of my own/game.rb
+++ Projects of my own/game.rb
@@ -1,7 +1,8 @@
 require 'pry-byebug'
 
 class Vikings
-    attr_accessor :health, :attack_power, :armour, :name
+    attr_accessor :name, :armour,:health 
+    attr_reader :attack_power
     @@id=0
     
     def initialize(name, attack_power, armour)
@@ -42,11 +43,20 @@ class Vikings
         false
       end
     end
+
+    def self.id_number
+        "This is the latest id number: #{@@id}\n\n"
+    end
 end
   
+def turn(p1,p2)
+    p1.attack(p2)
+    p2.attack(p1)
+end
+
 # We can add subclasses that inherit the properties of viking, like saxon that have their own armour and attack points later.
 # Can add items and different attacks and counter attacks
-#   can add accuracy
+#   can add accuracy or probablitiy for the attack to miss
 
 puts "Enter your name(Player 1): "
 name = gets.chomp
@@ -61,10 +71,14 @@ armour = rand(1..5)
 player2 = Vikings.new(name, attack_power,armour)
   
 until (player1.is_dead? || player2.is_dead?)
-  player1.attack(player2)
-#   puts
-  player2.attack(player1)
-#   puts
+  puts "player 1 rolls the die and gets #{result_1=rand(1..6)}"
+  puts "player 2 rolls the die and gets #{result_2=rand(1..6)}"
+  puts
+  if result_1>result_2
+    turn(player1, player2)
+  else
+    turn(player2, player1)
+  end
 end
 
 puts "#{player1.name} is now dead and has lost the battle!" if player1.is_dead?
